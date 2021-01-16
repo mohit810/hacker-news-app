b3a214dd56975e779ae968c55c4bcc3ffb71ce20
Gh0sT <iammohit810@gmail.com>
2021-01-16 20:48:43 +0530

    complete

---
+++ SwiftUI-H4X0R News/Views/DetailView.swift
@@ -0,0 +1,23 @@
+//
+//  DetailView.swift
+//  SwiftUI-H4X0R News
+//
+//  Created by Mohit Yadav on 16/01/21.
+//
+
+import SwiftUI
+
+struct DetailView: View {
+    
+    let url: String?
+    
+    var body: some View {
+        WebView(urlString: url)
+    }
+}
+
+struct DetailView_Previews: PreviewProvider {
+    static var previews: some View {
+        DetailView(url: "https://www.google.com")
+    }
+}
