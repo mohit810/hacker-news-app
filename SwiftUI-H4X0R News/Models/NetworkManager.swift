c479f457902fe14061a75c381c41665270370682
Gh0sT <iammohit810@gmail.com>
2021-01-16 18:19:15 +0530

    initial commit

---
+++ SwiftUI-H4X0R News/Models/NetworkManager.swift
@@ -0,0 +1,14 @@
+//
+//  NetworkManager.swift
+//  SwiftUI-H4X0R News
+//
+//  Created by Mohit Yadav on 16/01/21.
+//
+
+import Foundation
+
+class NetworkManager {
+    func fetchData() {
+        
+    }
+}
