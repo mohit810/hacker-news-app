00358cd44896334dd83dbcd2a0476e441ceef187
Gh0sT <iammohit810@gmail.com>
2021-01-16 17:41:04 +0530

    Initial Commit

---
+++ SwiftUI-H4X0R News/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  SwiftUI-H4X0R News
+//
+//  Created by Mohit Yadav on 16/01/21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ SwiftUI-H4X0R News/SwiftUI_H4X0R_NewsApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUI_H4X0R_NewsApp.swift
+//  SwiftUI-H4X0R News
+//
+//  Created by Mohit Yadav on 16/01/21.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUI_H4X0R_NewsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
